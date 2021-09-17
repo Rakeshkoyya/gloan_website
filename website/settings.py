8b444e30be7c63c2d1e9a1ded8914be6cefaaa85
Rakeshkoyya <rakeshkoyya2024@gmail.com>
2021-09-17 22:15:46 +0530

    made ready for heroku

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*mjduji6m&ltfpwkf3rs8l#bnu&%*=b-$m4muq)ml30xub_8lo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gloan.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -135,11 +135,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
     ]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
